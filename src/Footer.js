5b2c14a4a2bd8c2c2244b12852acc695c877e5f2
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-16 20:22:44 +0100

    Test footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Social Media</p>
+            <p>Welcom Pati ;)</p>
         </div>
     );
 }
