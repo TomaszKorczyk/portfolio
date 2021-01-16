4bc332115b065779428308d9490385069c371988
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-16 19:24:43 +0100

    Test new componnent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
            <h1>My name is Tom</h1>
+           <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>My name is Tom</h1>
+        </div>
+    );
+}
+
+export default Header;
