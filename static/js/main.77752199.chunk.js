a4cb60ba0640b61745a143a4de224ae309be33fa
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-21 19:34:52 +0100

    Updates

--- static/js/main.77752199.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{2:function(o,t,e){"use strict";e.r(t);var r=e(1);e.n(r).a.render("Hello World!",document.getElementById("root"))}},[[2,1,2]]]);
-//# sourceMappingURL=main.77752199.chunk.js.map
---
+++ static/js/main.91b53505.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(t,e,o){"use strict";o.r(e);var n=o(0),i=(o(1),o(3)),s=o.n(i);o(9);var r=function(t){var e=t.title;return Object(n.jsx)("div",{className:"box",children:Object(n.jsx)("div",{children:e})})};s.a.render(Object(n.jsx)(r,{title:"Jak si\u0119 masz moja su\u0142tanko :* ?"}),document.getElementById("root"))},9:function(t,e,o){}},[[10,1,2]]]);
+//# sourceMappingURL=main.91b53505.chunk.js.map
