4b070ac8e959504ca47ef1717f994ff3108e05c5
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-24 18:19:40 +0100

    Edit toogle button

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {title: "Hello React 2"};
+        this.state = {title: "Hello React 2", isShowing: false};
     }
+// states are Immutable
 
     render() {
         return(
@@ -21,14 +22,20 @@ class App extends React.Component {
                 <div className="w-1/2">
                     <div className="text-center">
                         <div className="my-4 text-white">{this.state.title}</div>
-                        <button className="my-2 p-2 bg-white text-blue-600 font-bold tracking-normal rounded-full focus:outline-none">
+                        <button 
+                            className="my-2 p-2 bg-white text-blue-600 font-bold tracking-normal rounded-full focus:outline-none"
+                            onClick={() => this.setState({isShowing: !this.state.isShowing})}
+                        >
                             Toogle Image
                         </button>
                     </div>
+                    {
+                        this.state.isShowing ?
                         <img 
                             src="https://images.unsplash.com/photo-1533422902779-aff35862e462?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
                             alt="Bridge on the forest"
-                        />
+                        /> : null
+                    }
                 </div>
             </section>
         );        
