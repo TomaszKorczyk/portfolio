3d240859cc5e125c9f4a9964a62ea6e834a98312
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-25 09:04:33 +0100

    Edit Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,14 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Images extends Component {
-    render() {
-        return (
-            <img 
-                src="https://images.unsplash.com/photo-1533422902779-aff35862e462?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
-                alt="Bridge on the forest"
-            />
+export default function Images() {
+    
+    const [myInterval, setmyInterval] = useState(null);
+
+    useEffect(() => {
+        console.log("Images Comp Mounted");
+        setmyInterval(setInterval(() => {
+            console.log("Hello");
+            }, 1000)
         );
-    }
+
+        return () => {
+            clearInterval(myInterval);
+            console.log("Images Unmount");
+        }
+    }, []);
+
+    return (
+        <img 
+            src="https://images.unsplash.com/photo-1533422902779-aff35862e462?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
+            alt="Bridge on the forest"
+        />
+    );
 }
 
-export default Images;
+
+// class Images extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = { interval: null};
+//     }
+
+//     componentDidMount(){
+//         console.log("Images Comp Mounted");
+//         this.setState({
+//             interval: setInterval(() =>{
+//                 console.log("Hello");
+//             }, 1000),
+//         });
+//     }
+
+//     componentWillUnmount(){
+//         console.log("Images Comp Unmounted");
+//         clearInterval(this.state.interval);
+//     }
+
+//     render() {
+//         return (
+//             <img 
+//                 src="https://images.unsplash.com/photo-1533422902779-aff35862e462?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
+//                 alt="Bridge on the forest"
+//             />
+//         );
+//     }
+// }
