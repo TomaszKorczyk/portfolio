e32e6a787a0a36d1f5b0eb77fc647e42a89121cb
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-02-09 11:24:43 +0100

    Update  add file Header.js and clear App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import "./assets/css/tailwind.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./utils/routes";
+import Header from "./components/Header";
 
 function App() {
-    return <Router>
-        <Switch>
-            {routes.map((route) => (
-                    <Route
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.component}
-                    />
-                ))
-            }
-        </Switch>
-    </Router>
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                {routes.map((route) => (
+                        <Route
+                            path={route.path}
+                            exact={route.exact}
+                            component={route.component}
+                        />
+                    ))
+                }
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
