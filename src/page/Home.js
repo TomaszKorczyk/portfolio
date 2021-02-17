ccc8aae7a519e7bc7e13f121e1c320537406a37f
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-02-17 13:16:50 +0100

    Update Install frame-motion

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
     return( 
         <div className="flex h-screen text-white text-3xl font-bold">
-            <h1 className="m-auto">Home</h1>
+            <motion.h1 
+                initial={{opacity: 0, x:-300}}
+                animate={{
+                    scale: 1.5, 
+                    opacity: 1, 
+                    x:0, 
+                    transition:{duration:1}, 
+                    rotate: 720
+                }}
+                className="m-auto"
+            >
+                Welcome Home
+            </motion.h1>
         </div>
     );
 }
