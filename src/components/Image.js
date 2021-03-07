62437e1c2d5f56528158dae657a65f52456e6769
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-03-07 15:04:08 +0100

    Update Images section

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,44 +1,25 @@
-import { motion } from 'framer-motion';
 import React, { useState } from 'react'
 
-export default function Image({index, image, handleRemove}) {
+export default function Image({index, image, handleRemove, show}) {
     const [isHovering, setIsHovering] = useState(false);
-    const [showPreview, setShowPreview] = useState(false);
+    
     return (
-        <div className="w-1/3 p-2 border flex justify-center">
-            <div 
-                className="relative"
-                onMouseEnter={()=>setIsHovering(true)}
-                onMouseLeave={()=>setIsHovering(false)}
-            >
-                <i 
-                    className={`fas fa-times absolute px-0.5 py-0.4 bg-black rounded right-0 cursor-pointer opacity-25 hover:opacity-100 
-                    ${isHovering ? "" : "hidden"}`}
-                    onClick={() => handleRemove(index)}
-                ></i>
-                <img
-                    onClick={() => setShowPreview(true)}
-                    className="h-auto"
-                    src={image} 
-                    alt=""
-                />
-            </div>
-            {showPreview && (
-                <section 
-                    className="fixed w-full h-full flex justify-center items-center top-0 left-0 z-40"
-                    onClick={() => setShowPreview(false)}
-                >
-                    <div className="bg-white text-black">
-                        <img
-                            src={image}
-                            className="rounded"
-                            width="300"
-                            height="auto" 
-                            alt=""
-                        />
-                    </div>
-                </section>
-            )}
+        <div 
+            className="relative"
+            onMouseEnter={()=>setIsHovering(true)}
+            onMouseLeave={()=>setIsHovering(false)}
+        >
+            <i 
+                className={`fas fa-times absolute px-0.5 py-0.4 bg-black rounded right-0 cursor-pointer opacity-25 hover:opacity-100 
+                ${isHovering ? "" : "hidden"}`}
+                onClick={() => handleRemove(index)}
+            ></i>
+            <img
+                onClick={show}
+                className="h-auto"
+                src={image} 
+                alt=""
+            />
         </div>
     );
 }
--- src/components/Images.js
+++ src/components/Images.js
@@ -4,6 +4,7 @@ import useFetchImage from '../utils/hooks/useFetchImage';
 import Loading from './Loading';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import useDebounce from '../utils/hooks/useDebounce';
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 
 export default function Images() {
     const [page, setPage] = useState(1);
@@ -26,22 +27,53 @@ export default function Images() {
     }
 
     function ShowImage() {
+        const [showPreview, setShowPreview] = useState(false);
         return (
-            <InfiniteScroll 
-                dataLength={images.length} 
-                next={() => setPage(page + 1)}
-                hasMore={true}
-                className="flex flex-wrap"
-            > 
-                {images.map((img, index) => ( 
-                    <Image 
-                        image={img.urls.regular} 
-                        handleRemove={handleRemove} 
-                        index={index}
-                        key={index}
-                    />
-                ))}
-            </InfiniteScroll>
+            <AnimateSharedLayout>
+                <InfiniteScroll 
+                    dataLength={images.length} 
+                    next={() => setPage(page + 1)}
+                    hasMore={true}
+                    className="flex flex-wrap"
+                > 
+                    {images.map((img, index) => ( 
+                        <motion.div 
+                            className="w-1/3 p-2 border flex justify-center"
+                            key={index}
+                            layoutId={img.urls.regular}
+                            initial={{ opacity : 0 }}
+                            animate={{ opacity : 1 }}
+                        >
+                            <Image
+                                show = {()=> setShowPreview(img.urls.regular)}
+                                image={img.urls.regular} 
+                                handleRemove={handleRemove} 
+                                index={index}
+                            />
+                        </motion.div>
+                    ))}
+                </InfiniteScroll>
+                <AnimatePresence>
+                    {showPreview && (
+                        <motion.section
+                            layoutId={showPreview}
+                            exit={{ opacity: 0,  rotate: 360, transition: {duration: 0.5} }}
+                            className="fixed w-full h-full flex justify-center items-center top-0 left-0 z-40"
+                            onClick={() => setShowPreview(false)}
+                        >
+                            <div className="bg-white text-black">
+                                <img
+                                    src={showPreview}
+                                    className="rounded-lg"
+                                    width="300"
+                                    height="auto" 
+                                    alt=""
+                                />
+                            </div>
+                        </motion.section>
+                    )}
+                </AnimatePresence>
+            </AnimateSharedLayout>
         );
     }
 
